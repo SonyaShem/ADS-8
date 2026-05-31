93de8ad013873e5f5d7eb0e3a08b1b11f89fcf72
SonyaShem <sonyasha2101@gmail.com>
2026-05-31 17:39:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,42 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <algorithm>
 #include  "bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+    std::string currentWord = "";
+    int ch;
+    while ((ch = file.get()) != EOF) {
+        if (std::isalpha(ch)) {
+            currentWord += std::tolower(ch);
+        } else {
+            if (!currentWord.empty()) {
+                tree.insert(currentWord);
+                currentWord = "";
+            }
+        }
+    }
+    if (!currentWord.empty()) {
+        tree.insert(currentWord);
+    }
+    file.close();
+}
+bool compareNodes(const BST<std::string>::Node* a, const BST<std::string>::Node* b) {
+    if (a->count != b->count) return a->count > b->count;
+    return a->key < b->key;
+}
+void printFreq(BST<std::string>& tree) {
+    std::vector<BST<std::string>::Node*> wordList = tree.getAllNodes();
+    std::sort(wordList.begin(), wordList.end(), compareNodes);
+    std::ofstream outFile("result/freq.txt");
+    for (size_t i = 0; i < wordList.size(); ++i) {
+        std::cout << wordList[i]->key << " : " << wordList[i]->count << std::endl;
+        outFile << wordList[i]->key << " : " << wordList[i]->count << "\n";
+    }
+    outFile.close();
 }
